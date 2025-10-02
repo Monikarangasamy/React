51f70db2e44b6fa506f4769519f43d480639c15c
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-10-02 12:11:02 +0530

    Update Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,47 +4,47 @@ import QUESTIONS from '../data/questions';
 import { QUIZ_STAGES } from '../constants/constant';
 
 const Quiz = ({ score, setScore, setQuizStage }) => {
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [selectedOption, setSelectedOption] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-	const onNextClick = () => {
-		if (selectedOption === QUESTIONS[currentQuestion].correctOption) {
-			setScore(score + 1);
-		}
+  const onNextClick = () => {
+    if (selectedOption === QUESTIONS[currentQuestion].correctOption) {
+      setScore(score + 1);
+    }
 
-		if (currentQuestion === QUESTIONS.length - 1) {
-			setQuizStage(QUIZ_STAGES.ENDED);
-		}
-		setCurrentQuestion(currentQuestion + 1);
-		setSelectedOption(null);
-	};
+    if (currentQuestion === QUESTIONS.length - 1) {
+      setQuizStage(QUIZ_STAGES.ENDED);
+    }
+    setCurrentQuestion(currentQuestion + 1);
+    setSelectedOption(null);
+  };
 
-	return (
-		<div className='quiz-wrapper'>
-			<div className='header'>
-				<p>
-					{currentQuestion + 1} of {QUESTIONS.length} questions
-				</p>
-			</div>
-			<div className='quiz'>
-				<p className='question'>
-					{QUESTIONS[currentQuestion].question}
-				</p>
-				{QUESTIONS[currentQuestion].options.map((option) => (
-					<Option
-						key={option}
-						data={option}
-						correctOption={QUESTIONS[currentQuestion].correctOption}
-						selectedOption={selectedOption}
-						setSelectedOption={setSelectedOption}
-					/>
-				))}
-			</div>
-			<button id='next' onClick={onNextClick}>
-				Next
-			</button>
-		</div>
-	);
+  return (
+    <div className='quiz-wrapper'>
+      <div className='header'>
+        <p>
+          {currentQuestion + 1} of {QUESTIONS.length} questions
+        </p>
+      </div>
+      <div className='quiz'>
+        <p className='question'>
+          {QUESTIONS[currentQuestion].question}
+        </p>
+        {QUESTIONS[currentQuestion].options.map((option) => (
+          <Option
+            key={option}
+            data={option}
+            correctOption={QUESTIONS[currentQuestion].correctOption}
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+          />
+        ))}
+      </div>
+      <button id='next' onClick={onNextClick}>
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Quiz;
