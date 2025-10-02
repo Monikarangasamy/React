ee78afc0c25fc24cba7212782e9ed94287c58ceb
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-10-02 11:37:53 +0530

    Add files via upload

---
+++ src/components/Option.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Option = ({ data, correctOption, selectedOption, setSelectedOption }) => {
+	return (
+		<button
+			className={`option ${
+				selectedOption &&
+				(data === correctOption
+					? 'correct'
+					: data === selectedOption
+					? 'incorrect'
+					: '')
+			}`}
+			onClick={() => setSelectedOption(data)}
+			disabled={!!selectedOption}
+		>
+			{data}
+		</button>
+	);
+};
+
+export default Option;
---
+++ src/components/Start.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { QUIZ_STAGES } from '../constants/constant';
+
+const Start = ({ setQuizStage }) => {
+	return (
+		<div className='start-screen'>
+			<h1>Ready for a Quiz?</h1>
+			<button
+				id='start'
+				onClick={() => setQuizStage(QUIZ_STAGES.IN_PROGRESS)}
+			>
+				Start
+			</button>
+		</div>
+	);
+};
+
+export default Start;
