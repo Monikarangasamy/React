680955e1ad68e9c5e3c4eb727f840a981cc2fa55
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-10-02 12:11:47 +0530

    Update Result.jsx

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { QUIZ_STAGES } from '../constants/constant';
 
 const Result = ({ score, setScore, setQuizStage }) => {
-	const onRestart = () => {
-		setScore(0);
-		setQuizStage(QUIZ_STAGES.IN_PROGRESS);
-	};
+  const onRestart = () => {
+    setScore(0);
+    setQuizStage(QUIZ_STAGES.IN_PROGRESS);
+  };
 
-	return (
-		<div className='result-screen'>
-			<div className='score'>Your Score: {score}</div>
-			<button id='restart' onClick={onRestart}>
-				Restart
-			</button>
-		</div>
-	);
+  return (
+    <div className='result-screen'>
+      <div className='score'>Your Score: {score}</div>
+      <button id='restart' onClick={onRestart}>
+        Restart
+      </button>
+    </div>
+  );
 };
 
 export default Result;
+
+
