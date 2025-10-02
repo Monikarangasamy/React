487c18ba2aa6e758cbd2e71af329cc32fae4ef58
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-10-02 12:00:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,28 @@ import Result from './components/Result';
 import { QUIZ_STAGES } from './constants/constant';
 
 const App = () => {
-	const [quizStage, setQuizStage] = useState(QUIZ_STAGES.START);
-	const [score, setScore] = useState(0);
+  const [quizStage, setQuizStage] = useState(QUIZ_STAGES.START);
+  const [score, setScore] = useState(0);
 
-	return (
-		<div>
-			{quizStage === QUIZ_STAGES.START ? (
-				<Start setQuizStage={setQuizStage} />
-			) : quizStage === QUIZ_STAGES.IN_PROGRESS ? (
-				<Quiz
-					score={score}
-					setScore={setScore}
-					setQuizStage={setQuizStage}
-				/>
-			) : (
-				<Result
-					score={score}
-					setScore={setScore}
-					setQuizStage={setQuizStage}
-				/>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {quizStage === QUIZ_STAGES.START ? (
+        <Start setQuizStage={setQuizStage} />
+      ) : quizStage === QUIZ_STAGES.IN_PROGRESS ? (
+        <Quiz
+          score={score}
+          setScore={setScore}
+          setQuizStage={setQuizStage}
+        />
+      ) : (
+        <Result
+          score={score}
+          setScore={setScore}
+          setQuizStage={setQuizStage}
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;
