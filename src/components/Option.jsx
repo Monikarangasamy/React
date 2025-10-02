573c10a8e81bd8f1cf50f76cc121f62ebae1b1b9
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-10-02 12:09:38 +0530

    Update Option.jsx

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 const Option = ({ data, correctOption, selectedOption, setSelectedOption }) => {
-	return (
-		<button
-			className={`option ${
-				selectedOption &&
-				(data === correctOption
-					? 'correct'
-					: data === selectedOption
-					? 'incorrect'
-					: '')
-			}`}
-			onClick={() => setSelectedOption(data)}
-			disabled={!!selectedOption}
-		>
-			{data}
-		</button>
-	);
+  return (
+    <button
+      className={`option ${
+        selectedOption &&
+        (data === correctOption
+          ? 'correct'
+          : data === selectedOption
+          ? 'incorrect'
+          : '')
+      }`}
+      onClick={() => setSelectedOption(data)}
+      disabled={!!selectedOption}
+    >
+      {data}
+    </button>
+  );
 };
 
 export default Option;
+
+
